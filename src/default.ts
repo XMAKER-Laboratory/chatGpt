a34ed168b5950a24d1ba693eaccfe34e22b67db1
XMAKER-Laboratory <127171575+XMAKER-Laboratory@users.noreply.github.com>
2023-03-09 01:03:26 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,6 +8,5 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - ChatGPT带KEY成品账号(亲测可用)， [点击这里获取账号KEY](https://dwz.cn/RxMxlcaq)
-- 【使用说明】最下方点击设置，输入您的API Key就可以进行聊天对答了。
-- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat) 和 [Vercel](http://vercel.com/) 提供支持。
-- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 使用的时候最下方点击设置，输入您的账号Key就可以进行聊天对答了。
+- Shift+Enter换行，开头输入/或者空格搜索Prompt预设。`
