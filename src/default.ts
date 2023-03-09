9bc1f539e93c0d7b40d95dd453b3baa9180d21b8
XMAKER-Laboratory <127171575+XMAKER-Laboratory@users.noreply.github.com>
2023-03-10 01:29:15 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -9,4 +9,4 @@ export const defaultSetting = {
 export const defaultMessage = `
 - ChatGPT带KEY成品账号(亲测可用)， [点击这里获取账号KEY](https://dwz.cn/RxMxlcaq)
 - 使用的时候最下方点击设置，输入您的账号Key就可以进行聊天对答了。
-- Shift+Enter换行，开头输入/或者空格搜索Prompt预设。`
+- Shift+Enter换行，开头输入/或者空格搜索可选择对应的预设。`
